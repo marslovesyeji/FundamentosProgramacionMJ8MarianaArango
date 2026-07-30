ebdcaafe3c871205fa8c7aac37dc7b7b2e240a10
marslovesyeji <mariana.arangoa@upb.edu.co>
2026-07-30 08:15:18 -0500

    Primer commit

---
+++ VariablesConstantes/VariablesConstantes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VariablesConstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Mariana";
+            Console.WriteLine("Bienvenido "+ nombre+" al curso de Fundamentos de Programación.");
+        }
+    }
+}
