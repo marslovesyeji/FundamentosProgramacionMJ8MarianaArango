983615fa28bdb299b96b2f90e78fd187c228df77
marslovesyeji <mariana.arangoa@upb.edu.co>
2026-08-18 09:39:05 -0500

    Condicionales Múltiples

---
+++ 6.CondicionalesMultiples/6.CondicionalesMultiples/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _6.CondicionalesMultiples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int respuesta = 0;
+            Console.WriteLine("------------Menú------------");
+            Console.WriteLine("1. Opción 1     2. Opción 2");
+            Console.WriteLine("3. Opción 3     4. Opción 4");
+            Console.WriteLine("5. Opción 5");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Elija una opción");
+            respuesta = int.Parse(Console.ReadLine());
+
+            switch (respuesta)
+            {
+                case 1:
+                    Console.WriteLine("Eligió la opción 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Eligio la opción 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Eligio la opción 3");
+                    break;
+                case 4:
+                    Console.WriteLine("Eligio la opción 4");
+                    break;
+                case 5:
+                    Console.WriteLine("Eligio la opción 5");
+                    break;
+                default:
+                    Console.WriteLine("Elija una opción válida");
+                    break;
+            }
+        }
+    }
+}
