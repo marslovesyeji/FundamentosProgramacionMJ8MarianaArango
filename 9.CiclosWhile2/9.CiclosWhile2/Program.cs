a9a740f47b194b6a2c9403d23322321a2d529bde
marslovesyeji <mariana.arangoa@upb.edu.co>
2026-08-27 09:29:28 -0500

    Ejercicio Clase - While

---
+++ 9.CiclosWhile2/9.CiclosWhile2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _9.CiclosWhile2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Realizar un algoritmo que le pida al ususario ingresar por teclado números enteros al azar. Al usuario le corresponde ingresar la cantidad de número que va a introducir
+            //El algoritmo debe escribir en pantalla:
+            // La cantidad de números introducidos que son mayores que 0
+            // La cantidad de números introducidos menores que 0
+            // La cantidad de números iguales a 0
+            int cnum = 0;
+            int num = 0;
+            int numin = 0; //Contador números ingresados
+            int cm = 0; //Contador números mayores
+            int cme = 0; //Contador números menores
+            int cmi = 0; //Contador números iguales
+            Console.WriteLine("Ingrese la cantidad de números que va a introducir:");
+            cnum = int.Parse(Console.ReadLine());
+            while (numin < cnum)
+            {
+                numin++;
+                Console.WriteLine("Ingrese el número");
+                num = int.Parse(Console.ReadLine());
+                if (num > 0)
+                {
+                    cm++;
+                }
+                else
+                {
+                    if (num < 0)
+                    {
+                        cme++;
+                    }
+                    else
+                    {
+                        cmi++;
+                    }
+                }
+            }
+            Console.WriteLine("La cantidad de números mayores a 0 ingresados son: " + cm);
+            Console.WriteLine("La cantidad de números menores a 0 ingresados son: " + cme);
+            Console.WriteLine("La cantidad de números iguales a 0 ingresados son: " + cmi);
+        }
+    }
+}
---
+++ 9.CiclosWhile2/9.CiclosWhile2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("9.CiclosWhile2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("9.CiclosWhile2")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("382bbb0a-43ed-4d38-baf5-0d9b297f0941")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
