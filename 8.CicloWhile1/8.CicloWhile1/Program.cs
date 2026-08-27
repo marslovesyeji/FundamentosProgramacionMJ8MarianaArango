28100f89d1e766c080c83e42263b9b8d4f2640d6
marslovesyeji <mariana.arangoa@upb.edu.co>
2026-08-27 09:30:01 -0500

    Solución Tarea While

---
+++ 8.CicloWhile1/8.CicloWhile1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _8.CicloWhile1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Realizar un algoritmo que pida números enteros positivos y los sume, hasta que se ingrese un número entero negativo. Se debe mostrar por pantalla el total de la suma de los números integrados.
+            int sumaEP = 0; //acumulador
+            int num = 0;
+            Console.WriteLine("Ingrese un número para sumar. Ingrese -1 para terminar.");
+            num = int.Parse(Console.ReadLine());
+            while (num>=0)
+            {
+                sumaEP += num;
+                Console.WriteLine("Ingrese un número para sumar. Ingrese -1 para terminar.");
+                num = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("La suma de los números positivos ingresados es: " + sumaEP);
+
+
+        }
+    }
+}
