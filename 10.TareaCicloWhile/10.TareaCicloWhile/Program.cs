bc7fe78beaedda741ab523c531e33a01e032d615
marslovesyeji <mariana.arangoa@upb.edu.co>
2026-09-03 09:26:39 -0500

    Tarea Ciclo While

---
+++ 10.TareaCicloWhile/10.TareaCicloWhile/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Random random = new Random();
+        int numse = random.Next(1, 101);
+        int intento = 0;
+
+        while (intento != numse)
+        {
+            Console.Write("Adivina el número (1 - 100): ");
+            intento = int.Parse(Console.ReadLine());
+
+            if (intento > numse)
+            {
+                Console.WriteLine("Demasiado alto.");
+            }
+            else if (intento < numse)
+            {
+                Console.WriteLine("Demasiado bajo.");
+            }
+            else
+            {
+                Console.WriteLine("¡Correcto! Has adivinado el número.");
+            }
+        }
+    }
+}
