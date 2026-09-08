1a7fb5c2a4b1800ba471ed667ab40a3982791804
marslovesyeji <mariana.arangoa@upb.edu.co>
2026-09-08 09:18:54 -0500

    14.Taller ciclos while

---
+++ 14.TallerCiclosWhiile/14.TallerCiclosWhiile/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace _14.TallerCicloWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float cal;
+            int cont = 0;
+            float acum=0;
+            int res;
+            float prom;
+            do
+            {
+                cont++;
+                Console.WriteLine("Ingrese su nota");
+                cal = float.Parse(Console.ReadLine());
+                Console.WriteLine("Si desea agregar otra nota presione 1, de lo contrario presione 2");
+                res = int.Parse(Console.ReadLine());
+                acum += cal;
+            }
+            while (res == 1);
+            prom = acum / cont;
+            Console.WriteLine($"El promedio de sus notas es:{prom}");
+            
+        }
+    }
+}
---
+++ 14.TallerCiclosWhiile/14.TallerCiclosWhiile/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("14.TallerCiclosWhiile")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("14.TallerCiclosWhiile")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("935bdd87-4164-4883-b3c6-a7e7f2bce78e")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
